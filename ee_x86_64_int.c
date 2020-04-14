7c879307e4af74ebdb09590e2e4e21e473b40800
Ragab Reda El-Kattawy <35061846+RagabEl-Kattawy@users.noreply.github.com>
2020-04-14 18:12:12 +0200

    Update ee_x86_64_int.c

--- ee_x86_64_int.c
+++ ee_x86_64_int.c
@@ -53,241 +53,103 @@
 
 #include "ee_internal.h"
 #include "ee_platform_config.h"
+#include <stdio.h>
 
-#define NUM_IDT_DESC            256
+#define OSEE_WIN32_INTERRUPTS_NO	32
+/* interrupt inital flag */
+typedef uint32_t OsEE_IntFlag_Type;
+OsEE_IntFlag_Type OsEE_Win32_IntFlags;
 
-#define IDT_ATTR_PRESENT        0x80
-#define IDT_ATTR_I386_INT_GATE  0xE
+/* interrupt interrupt flags buffer */
+typedef uint32_t OsEE_IntFlagBuffer_Type;
+OsEE_IntFlagBuffer_Type OsEE_Win32_IntFlagsBuffer;
 
-#define X86_TABLE_GDT           0
-#define X86_TABLE_LDT           1
+/* interrupt status */
+typedef unsigned char OsEE_IntState_Type;
+OsEE_IntState_Type OsEE_Win32_IntState;
 
-#define X86_PRIV_LEVEL_0        0
-#define X86_PRIV_LEVEL_1        1
-#define X86_PRIV_LEVEL_2        2
-#define X86_PRIV_LEVEL_3        3
+/* interrupt mask type */
+typedef uint32_t OsEE_IntMask_Type;
+OsEE_IntMask_Type OsEE_Win32_IntMask;
 
-#define INT_CAT_ISR1            1
-#define INT_CAT_ISR2            2
+/* interrupt index type */
+typedef unsigned char OsEE_Win32_IntIndex_Type;
 
-
-struct OsEE_x86_64_dtr {
-    uint16_t limit;
-    void*    base;
-} __attribute__((packed));
-
-struct OsEE_x86_64_idt {
-   uint16_t offset_1; /* offset bits 0..15 */
-   uint16_t selector; /* a code segment selector in GDT or LDT */
-   uint8_t ist;       /* bits 0..2 holds IST offset, rest of bits zero. */
-   uint8_t type_attr; /* type and attributes */
-   uint16_t offset_2; /* offset bits 16..31 */
-   uint32_t offset_3; /* offset bits 32..63 */
-   uint32_t zero;     /* reserved */
-} __attribute__((packed));
-/* static uint32_t idt[NUM_IDT_DESC * 4]; */
-static struct OsEE_x86_64_idt osEE_x86_64_idt_entry[NUM_IDT_DESC];
-
-typedef struct {
-  uintptr_t isr_hnd;
-  uint8_t   category;
-} OsEE_x86_64_lookup_table;
-static OsEE_x86_64_lookup_table osEE_isr_task_lookup_table[NUM_IDT_DESC];
-
-
-extern void osEE_x86_64_irq_base(void);
-
-
-static inline void osEE_x86_64_write_idtr(struct OsEE_x86_64_dtr *val)
+typedef enum
 {
-    asm volatile("lidt %0" : : "m" (*val));
-}
-/*...........................................................*/
-//we need to simulate this
-void osEE_x86_64_int_init(void)
+	WIN_32_FLSE,
+	WIN_32_TRUE
+}OsEE_Win32_Bool;
+
+void OsEE_Win32_InterruptHandler(int sig)
 {
-	//creat thread of the timer
-    struct OsEE_x86_64_dtr dtr;
+	OsEE_Win32_IntIndex_Type interrupt;
+	/* check incomming signal to terminate */
+   if (SIGTERM == sig)
+   {
+      /* Terminate Child process */
+     
+      /* wait for the second thread to finish */
+      pthread_join(osEE_x86_64_system_timer_thread, NULL);
+      /* kill Main process */
+      
+   }
+	
+	/* check if there is wainting interrupt flags */
+	while( Win32_ISRIsWaiting() )
+   	{
+	  /* get first waiting flag */
+      interrupt = Win32_GetFirstISR();
 
-    dtr.limit = sizeof(osEE_x86_64_idt_entry) - 1;
-    dtr.base = osEE_x86_64_idt_entry;
-
-    osEE_x86_64_write_idtr(&dtr);
-}
-/*...........................................................*/
-
-static void __attribute__((used)) osEE_x86_64_isr_wrapper(unsigned int vector)
-{
-    const OsEE_x86_64_lookup_table* const p_int =
-        &osEE_isr_task_lookup_table[vector];
-
-    if (p_int->category == INT_CAT_ISR1) {
-        OsEE_void_cb handler = (OsEE_void_cb)p_int->isr_hnd;
-        (*handler)();
-	//TODO !spurious
-        osEE_x86_64_int_send_eoi();
-    }
-    else
-        osEE_activate_isr2(p_int->isr_hnd);
+      /* only 0 .. 31 interrupts are allowed */
+      if (OSEE_WIN32_INTERRUPTS_NO > interrupt)
+      {
+		  /* handle if not masked & interrupt enable */
+         if ((OsEE_Win32_IntState) && ((OsEE_Win32_IntMask & (1 << interrupt ))  == 0 ))
+         {
+            //InterruptTable[interrupt]();
+         }
+         else /* buffer the flag otherwise */
+         {
+            OsEE_Win32_IntFlagsBuffer |= 1 << interrupt;
+         }
+      }
 }
 
+/* check for wainting nterrupts */
+ static OsEE_Win32_Bool Win32_ISRIsWaiting(void)
+ {
+	 if(0 != OsEE_Win32_IntFlags)
+	 {
+		 return WIN_32_TRUE;
+	 }
+	 else
+	 {
+		 return WIN_32_FALSE;
+	 }
+ }
 
-//simulate vector table
-//we will try to implement it ourselves
-static void osEE_x86_64_set_int_handler_(uint32_t  source_id,
-                             uint8_t   int_type,
-                             uintptr_t hnd)
+/* get the first waiting interrupt */
+static OsEE_Win32_IntIndex_Type Win32_GetFirstISR(void)
 {
-    OsEE_x86_64_lookup_table* const p_int_tag =
-        &osEE_isr_task_lookup_table[source_id];
+   	OsEE_Win32_IntIndex_Type ret = 255;
+   	OsEE_Win32_IntIndex_Type interrupt;
+   	uint8_t found = 0;
+	uint8_t mask = 0;
 
-    struct OsEE_x86_64_idt* const p_idt_entry =
-        &osEE_x86_64_idt_entry[source_id];
+   	for(interrupt = 0; (0 == found) && (OSEE_WIN32_INTERRUPTS_NO > interrupt); interrupt++)
+   	{
+	   mask = 1 << interrupt;
+      if (OsEE_Win32_IntFlags & mask)
+      {
+         found = 1;
+         ret = interrupt;
 
-    const unsigned long entry = (unsigned long)osEE_x86_64_irq_base
-                                + (source_id * 16);
+         /* reset interrupt flag */
+         OsEE_Win32_IntFlags &= ~mask;
+      }
+   }
 
-    p_idt_entry->offset_1 = entry & 0xffff;
-    p_idt_entry->selector = X86_TABLE_GDT | X86_PRIV_LEVEL_0 | CODE_SELECTOR_64;
-    p_idt_entry->ist = 0;
-    p_idt_entry->type_attr = IDT_ATTR_PRESENT | IDT_ATTR_I386_INT_GATE;
-    p_idt_entry->offset_2 = (entry >> 16) & 0xffff;
-    p_idt_entry->offset_3 = (entry >> 32) & 0xffffffff;
-/*
-    idt[source_id * 4] = (entry & 0xffff) | (INMATE_CS64 << 16);
-    idt[source_id * 4 + 1] = 0x8e00 | (entry & 0xffff0000);
-    idt[source_id * 4 + 2] = entry >> 32;
-*/
-
-    p_int_tag->category = int_type;
-    p_int_tag->isr_hnd = hnd;
+   return ret;
 }
-
-void set_interrupt_handler(uint32_t source_id, OsEE_void_cb callback)
-{
-  osEE_x86_64_set_int_handler_(source_id, INT_CAT_ISR1, (uintptr_t)callback);
-}
-
-void call_int(unsigned int source_id)
-{
-  osEE_x86_64_int_send_ipi(osEE_x86_64_get_core_id_raw(), source_id);
-}
-
-#if (defined(OSEE_API_DYNAMIC))
-StatusType osEE_hal_set_isr2_source
-(
-  OsEE_TDB * p_tdb,
-  ISRSource  source_id
-)
-{
-  osEE_x86_64_set_int_handler_(source_id, INT_CAT_ISR2, (uintptr_t)p_tdb->tid);
-  return E_OK;
-}
-#endif /* OSEE_API_DYNAMIC */
-
-//here we will modify
-FUNC(OsEE_bool, OS_CODE) osEE_cpu_startos(void)
-{
-//  OsEE_bool const continue_startos = osEE_std_cpu_startos();
-//
-//#if (!defined(OSEE_API_DYNAMIC))
-//  if (continue_startos == OSEE_TRUE) {
-//    size_t i;
-//#if (!defined(OSEE_SINGLECORE))
-//    CoreIdType const core_id = osEE_get_curr_core_id();
-//#endif /* !OSEE_SINGLECORE */
-///* Initialize ISRs of this core */
-//    OsEE_KDB * const p_kdb   = osEE_get_kernel();
-//
-//    for (i = 0U; i < (p_kdb->tdb_array_size - 1U); ++i) {
-//      /* ISR2 initialization */
-//      OsEE_TDB  * const p_tdb = (*p_kdb->p_tdb_ptr_array)[i];
-//
-//#if (!defined(OSEE_SINGLECORE))
-//      if (p_tdb->orig_core_id != core_id) {
-//        continue;
-//      } else
-//#endif /* !OSEE_SINGLECORE */
-//      if (p_tdb->task_type == OSEE_TASK_TYPE_ISR2) {
-//        osEE_x86_64_set_int_handler_(p_tdb->hdb.isr2_src, INT_CAT_ISR2, i);
-//      }
-//    }
-//// here the call of function timer init
-//
-//#if (defined(OSEE_HAS_SYSTEM_TIMER))
-//    osEE_x86_64_system_timer_init();
-//#endif /* OSEE_HAS_SYSTEM_TIMER */
-//  }
-//#endif /* !OSEE_API_DYNAMIC */
-//  return continue_startos;
-	//init signal handler
-	//call system timer init
-	//
-}
-
-#ifdef __x86_64__
-/*
- * First push/Last pop added for 16-byte alignment
- */
-asm(
-".macro irq_prologue vector\n\t"
-    "push %rdi\n\t"
-    "push %rdi\n\t"
-    "mov $vector,%rdi\n\t"
-    "jmp irq_common\n"
-".endm\n\t"
-
-
-".global osEE_x86_64_irq_base\n\t"
-".balign 16\n"
-
-"osEE_x86_64_irq_base: \n"
-"vector=0   \n"
-".rept 256   \n"
-    "irq_prologue vector    \n\t"
-    "vector = vector + 1    \n\t"
-    ".balign 16             \n\t"
-".endr                      \n"
-
-/*
- * **Issue TO BE FIXED **
- * In case of shared stack, osEE_x86_64_isr_wrapper can be
- * executed with not-aligned stack
- */
-"irq_common:    \n\t"
-    "push %rax  \n\t"
-    "push %rcx  \n\t"
-    "push %rdx  \n\t"
-    "push %rsi  \n\t"
-    "push %r8   \n\t"
-    "push %r9   \n\t"
-    "push %r10  \n\t"
-    "push %r11  \n\t"
-
-    "call osEE_x86_64_isr_wrapper  \n\t"
-
-    "pop %r11   \n\t"
-    "pop %r10   \n\t"
-    "pop %r9    \n\t"
-    "pop %r8    \n\t"
-    "pop %rsi   \n\t"
-    "pop %rdx   \n\t"
-    "pop %rcx   \n\t"
-    "pop %rax   \n\t"
-
-    "pop %rdi   \n\t"
-    "pop %rdi   \n\t"
-
-    "iretq"
-);
-
-
-//our modifycation
-
-void OSEE_InterruptHandler ()
-{
-
-}
-#else
-#error Not Implemented!
-#endif
+/* schedule buffered interrupts */
